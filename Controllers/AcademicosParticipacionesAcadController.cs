522c863819bd83dc64be57160018d8680b092191
GeronimoJauregui <geronimojaureguimartinez@gmail.com>
2019-12-29 03:09:15 -0600

    Esqueleto de los submodulos Comisiones,Participaciones academicas, publicaciones y reportes

---
+++ Controllers/AcademicosParticipacionesAcadController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SGCFIEE.Models;
+using System.Diagnostics;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SGCFIEE.Controllers
+{
+    public class AcademicosParticipacionesAcadController : Controller
+    {
+        // GET: /<controller>/
+        [Authorize]
+        public IActionResult Index()
+        {
+            ViewData["tipo"] = (int)HttpContext.Session.GetInt32("TipoUsuario");
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
