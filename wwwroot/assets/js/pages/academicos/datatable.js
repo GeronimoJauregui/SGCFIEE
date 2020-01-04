4f931a4c17c703a8780dac84b8ee6741e0cc8ff9
GeronimoJauregui <geronimojaureguimartinez@gmail.com>
2020-01-03 22:18:05 -0600

    datatable listos en todos los submodulos

---
+++ wwwroot/assets/js/pages/academicos/datatable.js
@@ -0,0 +1,40 @@
+﻿
+$(document).ready(function () {
+    /*Módulo Evaluaciones*/
+    $("#evaluacionCT").DataTable();
+    $("#evaluacionEstud").DataTable();
+    $("#evaluacionTutor").DataTable();
+    /*Módulo Producción*/
+    $("#MDidactico").DataTable();
+    $("#MPractica").DataTable();
+    $("#LibroEE").DataTable();
+    $("#proyectosE").DataTable();
+    /*Módulo Instructores de educación continua*/
+    $("#EduContinua").DataTable();
+    /*Módulo Coordinadores*/
+    $("#Academias").DataTable();
+    $("#programatransversal").DataTable();
+    /*Módulo ConsejoTecnico*/
+    $("#ConsejoTecnico").DataTable();
+    /*Módulo Publicaciones*/
+    $("#TablaArticulos").DataTable();
+    $("#TablaCongresos").DataTable();
+    $("#TablaRevistas").DataTable();
+    $("#TablaReportes").DataTable();
+    $("#TablaLibros").DataTable();
+    $("#TablaCapitulos").DataTable();
+    $("#TablaPatentes").DataTable();
+    $("#TablaTrabajos").DataTable();
+    $("#TablaConcursos").DataTable();
+    /*Módulo Participaciones académicas*/
+    $("#TablaAC").DataTable();
+    $("#TablaPonencias").DataTable();
+    $("#TablaCursos").DataTable();
+    $("#TablaProductos").DataTable();
+    /*Módulo Comisiones*/
+    $("#TablaJurOposicion").DataTable();
+    $("#TablaJurExperiencia").DataTable();
+    $("#TablaPreJurExperiencia").DataTable();
+    $("#TablaPLADEA").DataTable();
+    $("#TablaPartRedisenio").DataTable();
+});
