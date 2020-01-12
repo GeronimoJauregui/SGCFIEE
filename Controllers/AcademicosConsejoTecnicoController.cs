f66697ecad510570685a76adaa8a76ca118f0881
GeronimoJauregui <geronimojaureguimartinez@gmail.com>
2020-01-12 05:29:31 -0600

    Submodulo Consejo Tecnico listo

--- Controllers/AcademicosConsejoTecnicoController.cs
+++ Controllers/AcademicosConsejoTecnicoController.cs
@@ -5,10 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SGCFIEE.Models;
 using System.Diagnostics;
-
+using Fiver.Mvc.FileUpload.Models.Home;
 
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Authorization;
+using System.IO;
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace SGCFIEE.Controllers
@@ -20,20 +21,197 @@ namespace SGCFIEE.Controllers
         [Authorize]
         public IActionResult Index()
         {
+            List<TablaCT> ListCT = new List<TablaCT>();
             ViewData["tipo"] = (int)HttpContext.Session.GetInt32("TipoUsuario");
-            return View();
+            using (sgcfieeContext context = new sgcfieeContext())
+            {
+                ListCT = (from datos in context.ConsejoTecnico
+                                 join acad in context.Academicos on datos.IdAcademico equals acad.IdAcademicos
+                                 join perioIni in context.TipoPeriodo on datos.IdFechaInicial equals perioIni.IdPeriodo
+                                 join perioTer in context.TipoPeriodo on datos.IdFechaCierre equals perioTer.IdPeriodo
+                                 select new TablaCT
+                                 {
+                                     IdCT = datos.IdConsejoTecnico,
+                                     NumPersonal = acad.NumeroPersonal,
+                                     Nombre = acad.Nombre,
+                                     ApellidoPaterno = acad.ApellidoPaterno,
+                                     ApellidoMaterno = acad.ApellidoMaterno,
+                                     Archivo = datos.ArchivoActaA,
+                                     PeriodoInicio = perioIni.Nombre,
+                                     PeriodoTermino = perioTer.Nombre,
+                                     Status = acad.Status
+                                 }
+                               ).ToList();
+            }
+            return View(ListCT);
         }
         [Authorize]
         public IActionResult Crear()
         {
             ViewData["tipo"] = (int)HttpContext.Session.GetInt32("TipoUsuario");
+            using (sgcfieeContext context = new sgcfieeContext())
+            {
+                var acad = context.Academicos.ToList();
+                var perio = context.TipoPeriodo.ToList();
+                ViewData["academicos"] = acad;
+                ViewData["periodos"] = perio;
+            }
             return View();
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         [Authorize]
-        public IActionResult Editar()
+        public async Task<IActionResult> Guardar(IFormFile file, ConsejoTecnico datos)
+        {
+
+            using (sgcfieeContext context = new sgcfieeContext())
+            {
+                var ListCT= context.ConsejoTecnico.ToList();
+                foreach (ConsejoTecnico item in ListCT)
+                {
+                    if (datos.IdAcademico == item.IdAcademico && datos.IdFechaInicial == item.IdFechaInicial && datos.IdFechaCierre == item.IdFechaCierre)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                }
+                var new_name_table = datos.IdFechaInicial + "_" + datos.IdFechaCierre + "_" + datos.IdAcademico + "_" + file.GetFilename();
+                datos.ArchivoActaA = new_name_table;
+                context.ConsejoTecnico.Add(datos);
+                context.SaveChanges();
+            }
+
+            if (file == null || file.Length == 0)
+                return Content("file not selected");
+
+            var new_name_file = datos.IdFechaInicial + "_" + datos.IdFechaCierre + "_" + datos.IdAcademico + "_" + file.GetFilename();
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Archivos/ConsejoTecnico", new_name_file);
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            return RedirectToAction("Index");
+        }
+        [Authorize]
+        public IActionResult Editar(int id)
         {
             ViewData["tipo"] = (int)HttpContext.Session.GetInt32("TipoUsuario");
-            return View();
+            using (sgcfieeContext context = new sgcfieeContext())
+            {
+                ConsejoTecnico DatosCT = context.ConsejoTecnico.Where(s => s.IdConsejoTecnico == id).Single();
+                var perio = context.TipoPeriodo.ToList();
+                var acad = context.Academicos.ToList();
+                ViewData["periodos"] = perio;
+                ViewData["academicos"] = acad;
+                return View(DatosCT);
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> Actualizar(IFormFile file, ConsejoTecnico datos)
+        {
+            using (sgcfieeContext context = new sgcfieeContext())
+            {
+                var ListCT = context.ConsejoTecnico.ToList();
+
+                foreach (ConsejoTecnico item in ListCT)
+                {
+                    if (datos.IdAcademico == item.IdAcademico && datos.IdFechaInicial == item.IdFechaInicial && datos.IdFechaCierre == item.IdFechaCierre && datos.IdConsejoTecnico != item.IdConsejoTecnico)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                }
+            }
+            using (sgcfieeContext context = new sgcfieeContext())
+            {
+                if (file == null || file.Length == 0)
+                {
+                    var nomArchivo = context.ConsejoTecnico.Where(w => w.IdConsejoTecnico == datos.IdConsejoTecnico).Single();
+                    datos.ArchivoActaA = nomArchivo.ArchivoActaA;
+                }
+                else
+                {
+                    var new_name_table = datos.IdFechaInicial + "_" + datos.IdFechaCierre + "_" + datos.IdAcademico + "_" + file.GetFilename();
+                    datos.ArchivoActaA = new_name_table;
+                }
+
+            }
+            using (sgcfieeContext context = new sgcfieeContext())
+            {
+                context.ConsejoTecnico.Update(datos);
+                context.SaveChanges();
+                TempData["Mensaje"] = "La informacion se ha guardado correctamente";
+            }
+            if (file == null || file.Length == 0)
+            {
+                return RedirectToAction("Index");
+            }
+            var new_name_file = datos.IdFechaInicial + "_" + datos.IdFechaCierre + "_" + datos.IdAcademico + "_" + file.GetFilename();
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Archivos/ConsejoTecnico", new_name_file);
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult Eliminar(int id)
+        {
+            using (sgcfieeContext context = new sgcfieeContext())
+            {
+                ConsejoTecnico eliminar = context.ConsejoTecnico.Where(w => w.IdConsejoTecnico == id).Single();
+                context.ConsejoTecnico.Remove(eliminar);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+        public async Task<IActionResult> Descargar(string filename)
+        {
+
+            if (filename == null)
+                return Content("filename not present");
+
+            var path = Path.Combine(
+                           Directory.GetCurrentDirectory(),
+                           "wwwroot/Archivos/ConsejoTecnico", filename);
+
+            var memory = new MemoryStream();
+            using (var stream = new FileStream(path, FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+            memory.Position = 0;
+            return File(memory, GetContentType(path), Path.GetFileName(path));
+        }
+
+        private string GetContentType(string path)
+        {
+            var types = GetMimeTypes();
+            var ext = Path.GetExtension(path).ToLowerInvariant();
+            return types[ext];
+        }
+        private Dictionary<string, string> GetMimeTypes()
+        {
+            return new Dictionary<string, string>
+            {
+                {".txt", "text/plain"},
+                {".pdf", "application/pdf"},
+                {".doc", "application/vnd.ms-word"},
+                {".docx", "application/vnd.ms-word"},
+                {".xls", "application/vnd.ms-excel"},
+                {".xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"},
+                {".png", "image/png"},
+                {".jpg", "image/jpeg"},
+                {".jpeg", "image/jpeg"},
+                {".gif", "image/gif"},
+                {".csv", "text/csv"},
+                {".pptx","application/vnd.openxmlformats-officedocument.presentationml.presentation"},
+                {".rar","application/x-rar-compressed"},
+                {".zip","application/zip"}
+            };
         }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ Models/TablaCT.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace SGCFIEE.Models
+{
+    public class TablaCT
+    {
+        public int IdCT { get; set; }
+        public int? NumPersonal { get; set; }
+        public String Nombre { get; set; }
+        public String ApellidoPaterno { get; set; }
+        public String ApellidoMaterno { get; set; }
+        public String Archivo { get; set; }
+        public String PeriodoInicio { get; set; }
+        public String PeriodoTermino { get; set; }
+        public int? Status { get; set; }
+    }
+}
