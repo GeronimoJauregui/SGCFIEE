528ed3e1cde96cacd52ca4d91ef257ada0e65cde
GeronimoJauregui <geronimojaureguimartinez@gmail.com>
2020-05-04 19:00:57 -0500

    modificacion de la bd y nueva carpeta de scripts

---
+++ scrips_BDFIEE/Tipo_puesto.sql
@@ -0,0 +1,12 @@
+USE SGCFIEE;
+INSERT INTO Tipo_puesto (Nombre) VALUES
+("Director"),
+("Secretario de facultad"),
+("Enlace administrativo"),
+("Secretario de dirección"),
+("Coordinador de programa educativo"),
+("Coordinador de tutorías"),
+("Coordinador de académias"),
+("Administrador páginas web"),
+("Coordinador de educación continua"),
+("Maestro enlace");
---
+++ scrips_BDFIEE/tipos_laboratorios.sql
@@ -0,0 +1,11 @@
+USE SGCFIEE;
+
+INSERT INTO Tipo_Laboratorio (Nombre) VALUES
+("Laboratorio de electrónica básica"),
+("Laboratorio de tópicos avanzados"),
+("Laboratorio de transmisión de datos y redes"),
+("Laboratorio de pruebas mecánicas"),
+("Laboratorio de máquinas eléctricas"),
+("Laboratorio de control eléctrico y automatización"),
+("Laboratorio de electrónica"),
+("Laboratorio de mecatrónica");
